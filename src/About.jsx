8b3b0aab8ab54d018ce9f4ed62fcfe103a1da1c2
Rishita-bug <rishitabaghel@gmail.com>
2026-03-25 12:02:11 +0530

    links resume pdf

--- src/About.jsx
+++ src/About.jsx
@@ -3,7 +3,7 @@ import {useState} from 'react'
 import './About.css'
 import ProfileImg from './profile-img.jpg'
 import AboutHeader from './about-header.jpg'
-import Resume from './RishitaB-resume-edited.pdf'
+import Resume from './RishitaB_Resume.pdf'
 import ContactModal from './ContactModal'
 
 function AboutPage() { 
@@ -60,31 +60,15 @@ function AboutPage() {
           <p className= "rotate-link email-link" onClick = {handleCopy}>
             email
           </p>
-          <a className= "rotate-link resume-link" href="#" onClick={handleResumeClick}>
+          <a 
+            className= "rotate-link resume-link" 
+            href={Resume} 
+            target="_blank" 
+            rel="noopener noreferrer"
+          >
             resume
           </a>
         </div>
-        {/* <p className="para">
-        I’m a 2024 Communication Design graduate from 
-        <span className = "education"> National Institute </span> 
-        of Design, Andhra Pradesh with a fungal enthusiasm for 
-        <span className="work"> UX&nbsp;design and research. </span> 
-          During my time in Design school I stumbled upon visual design,
-          film, animation, graphic narrative, UX, branding and various
-          other offshoots of the discipline, making my approach to design
-        <span className="approach">process-driven, analytical yet rooted&nbsp;
-          in&nbsp;storytelling.</span>
-        </p> */}
-
-        {/* <p className="para">
-        Currently tinkering with <span className="lately">
-          web development, </span> and kindling a growing 
-          interest in graphic design for the web. As a creature
-           I am <span className="self">persistent, curious and wordy.</span> 
-           Indulgences include <span className="other">cortados, 
-            plein air scribbling, amateur poetry writing and watercolour painting </span> 
-            (sometimes concurrently).
-        </p> */}
 
         <p className = "para">
           <span className = "background">I’m a designer 
@@ -111,11 +95,18 @@ function AboutPage() {
           Indulgences include <span className = "other">treks, 
           cortados, plein air scribbling and amateur poetry writing.</span>
         </p>
+
       </div>
       <div className="links-container-mobile">
           <p className= "rotate-link-mobile"  onClick = {handleCopy}>email</p>
           <p className = {isCopied ? "tooltip tooltip-visible" : "tooltip"}>email copied to clipboard</p>
-          <a className= "rotate-link-mobile" href="#" onClick={handleResumeClick}>resume</a>
+          <a 
+            className = "rotate-link-mobile" 
+            href = "RishitaB_Resume.pdf" 
+            target = "_blank" 
+            rel = "noopener noreferrer">
+              resume
+          </a>
       </div>
       <p className = "about-footnote">
         Thanks for stopping by my digital abode! 
